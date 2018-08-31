25ee4dcaf33b25c26990db73cf3795d1a759fa8a
Hyeongdo Lee <mylovercorea@gmail.com>
2018-08-31 20:49:23 +0900

    [WIP] working sample

--- sample.py
+++ sample.py
@@ -2,18 +2,31 @@ from openpyxl import Workbook
 
 # Reference: https://openpyxl.readthedocs.io/en/stable/tutorial.html#create-a-workbook
 
-# A workbook is always created with at least one worksheet.
+# 워크북은 항상 최소한 하나의 워크시트를 갖고 있음
 wb = Workbook()
 
-# You can get it by using the
+# 활성화된 워크시트 가져 오기
 # openpyxl.workbook.Workbook.active()
 ws = wb.active
 
-# Create new worksheets
-ws1 = wb.create_sheet("My sheet1")      # insert at the end (default)
-ws2 = wb.create_sheet("My sheet2", 0)   # insert at first position
+# 새로운 워크시트 생성
+ws1 = wb.create_sheet("My sheet1")      # 가장 마지막 위치에 추가(기본값)
+ws2 = wb.create_sheet("My sheet2", 0)   # 가장 앞에 추가
 
-# Change sheet's title
+# 워크시트 제목 변경
+ws.title = "New Title"
 
-# Saving to a file
+# 워크시트 제목 탭 색상 변경
+ws.sheet_properties.tabColor = "1072BA"     # RRGGBB
+
+# 워크시트 제목을 설정 했으면 설정한 제목을 사용해서 워크시트를 가져올 수 있음
+ws3 = wb["New Title"]
+
+# 워크북에 있는 워크시트 이름 모두 가져오기 (openpyxl.workbook.Workbook.sheetnames())
+print(wb.sheetnames)
+
+for sheet in wb:
+    print(sheet.title)
+
+# 엑셀 파일 저장
 wb.save('sample.xlsx')
